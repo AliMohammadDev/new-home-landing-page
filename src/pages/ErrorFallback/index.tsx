8dcaaa4cfce01a7c8b82eefe1ed568f18a6d196e
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-01-18 13:07:57 +0300

    added local en er in 404 pages

--- src/pages/ErrorFallback/index.tsx
+++ src/pages/ErrorFallback/index.tsx
@@ -1,26 +1,36 @@
+import { useTranslation } from 'react-i18next';
 import { Link, useRouteError } from 'react-router-dom';
 
 const ErrorFallback = () => {
   const error = useRouteError();
+  const { t, i18n } = useTranslation();
+  const isRTL = i18n.language === 'ar';
 
   return (
-    <section className="flex h-svh items-center p-16 bg-[#EDEAE2] font-[Expo-arabic]">
+    <section
+      dir={isRTL ? 'rtl' : 'ltr'}
+      className="flex h-svh items-center p-16 bg-[#EDEAE2] font-[Expo-arabic]"
+    >
       <div className="container mx-auto my-8 flex flex-col items-center justify-center px-5">
         <div className="max-w-md text-center">
           <h2 className="mb-8 text-9xl font-extrabold">
-            <span className="sr-only">Error</span>404
+            <span className="sr-only">{t('ErrorFallback.title')}</span>
+            404
           </h2>
+
           <p className="text-2xl font-semibold md:text-3xl">
-            Sorry, we couldn't find this page.
+            {t('ErrorFallback.notFound')}
           </p>
+
           <p className="mb-8 mt-4 text-gray-800">
-            But dont worry, you can find plenty of other things on our homepage.
+            {t('ErrorFallback.description')}
           </p>
+
           <Link
             to="/"
             className="font-[Expo-bold] rounded px-8 py-3 bg-[#025043] text-white hover:bg-[#015045] transition"
           >
-            Back to homepage
+            {t('ErrorFallback.backHome')}
           </Link>
         </div>
       </div>
