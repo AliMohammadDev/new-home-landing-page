92b7554c899d44d64a94c6bb8ffaee7bfba56d9b
AliMohammadDev <aloshmohammad2001@gmail.com>
2026-01-21 15:27:03 +0300

    fixed in slider 1

--- src/components/Products/ProductSlider1.jsx
+++ src/components/Products/ProductSlider1.jsx
@@ -97,8 +97,9 @@ function ProductSlider1({ products = [] }) {
     prevArrow: <CustomArrow direction="prev" />,
     afterChange: (current) => setCurrentSlide(current),
     responsive: [
-      { breakpoint: 1280, settings: { slidesToShow: 3 } },
-      { breakpoint: 1024, settings: { slidesToShow: 2 } },
+      { breakpoint: 1280, settings: { slidesToShow: 4 } },
+      { breakpoint: 1024, settings: { slidesToShow: 4 } },
+      { breakpoint: 768, settings: { slidesToShow: 3 } },
       { breakpoint: 640, settings: { slidesToShow: 1 } },
     ],
   };
